6f44f4d0af6f71ab030e5980fc0d9928668bed63
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-22 12:05:50 +0200

    Erwan -removed exess output

--- multiple_tests.sh
+++ multiple_tests.sh
@@ -5,20 +5,20 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-echo "       " | ./$1
+echo "       " | ./$1 >/dev/null 2>&1
 echo "\"       \"" $? "the result should be 0"
 echo " "
-echo "ls" | ./$1
+echo "ls" | ./$1 >/dev/null 2>&1
 echo "\"ls\"" $? "the result should be 0"
 echo " "
-echo "ls /aaaaaa" | ./$1
+echo "ls /aaaaaa" | ./$1 >/dev/null 2>&1
 echo "\"ls /aaaaaaa\"" $? "the result should be 2"
 echo " "
-echo "thiscommanddoesntexist" | ./$1
+echo "thiscommanddoesntexist" | ./$1 >/dev/null 2>&1
 echo "\"thiscommanddoesntexist\"" $? "the result should be 127"
 echo " "
-echo "exit" | ./$1
+echo "exit" | ./$1 >/dev/null 2>&1
 echo "\"exit\"" $? "the result should be 0"
 echo " "
-echo "/bin/ls /var" | ./$1
+echo "/bin/ls /var" | ./$1 >/dev/null 2>&1
 echo "\"/bin/ls /var\"" $? "the result should be 0"
