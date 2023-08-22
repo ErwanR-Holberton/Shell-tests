faa68013cf43a9cf5c7a8d76e955aef070f61baf
ErwanR-Holberton <6218@holbertonstudents.com>
2023-08-22 10:31:44 +0200

    Erwan - added test

--- multiple_tests.sh
+++ multiple_tests.sh
@@ -16,3 +16,9 @@ echo "\"ls /aaaaaaa\"" $? "the result should be 2"
 echo " "
 echo "thiscommanddoesntexist" | ./$1
 echo "\"thiscommanddoesntexist\"" $? "the result should be 127"
+echo " "
+echo "exit" | ./$1
+echo "\"exit\"" $? "the result should be 0"
+echo " "
+echo "/bin/ls /var" | ./$1
+echo "\"/bin/ls /var\"" $? "the result should be 0"
